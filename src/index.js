e1695ccadb59fe514dcf530317f286a20313effb
habib6bd <habib6bd@gmail.com>
2022-10-07 02:30:29 +0600

    Initail commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const title = "To Do Title"
+const description = "This is my description. Here I have everyting i have input"
+const date = new Date();
+const dateName = date.getDate();
+const dateMonth = date.getMonth();
+const currentYear = date.getFullYear();
+console.log(date);
+
+// const headingStyle = {
+//     backgroundColor: "purple",
+//     padding: "15px",
+//     fontSize: "2em",
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <div>
+    <h1 className='headingStyle'>Welcome to My Family</h1> 
+    <h3>{title}</h3>
+    <p>{description}</p>
+    <p>{dateName + "/" + dateMonth +"/" + currentYear}</p>
+    </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
